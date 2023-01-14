20e6dc8aa8e6f2a3859d127ae8d42fc3ee972070
Juan Quino <quinojuan@gmail.com>
2023-01-14 08:36:38 -0300

    integrando cloudinary

--- client/src/components/UploadImage.jsx
+++ client/src/components/UploadImage.jsx
@@ -1,11 +1,32 @@
-import "./UploadImage.css"
+import { useState } from "react";
+import "./UploadImage.css";
+import {Cloudinary} from "@cloudinary/url-gen";
 
 const UploadImage = () => {
-  return (
-    <div className='upload'>
-      <button>UPLOAD Images</button>
-    </div>
-  )
-}
+  const [images, setImages] = useState([]);
+  const [imageToRemove, setImageToRemove] = useState(null);
 
-export default UploadImage
+  const handleRemoveImg = () => {};
+
+  const handleOpenWidget = () => {
+    var myWidget = Cloudinary.createUploadWidget(
+      {
+        cloudName: "dumzvr9ih",
+        uploadPreset: "fzl9vlnn",
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          console.log("Done! Here is the image info: ", result.info);
+        }
+      }
+    );
+  };
+
+  return (
+    <div className="upload">
+      <button onClick={() => handleOpenWidget()}>UPLOAD Images</button>
+    </div>
+  );
+};
+
+export default UploadImage;
