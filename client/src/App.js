95c40b8d78f12271047000370e3477d0980ab517
Juan Quino <quinojuan@gmail.com>
2023-01-14 00:16:23 -0300

    nuevo componente para subir imagenes en camino

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
+import UploadImage from "./components/UploadImage";
 import { useState } from "react";
-import axios from "axios"
+import axios from "axios";
 
 function App() {
-
   const [nft, setNft] = useState({
     nftName: "",
     clientSecret: "",
@@ -18,76 +18,79 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await axios.put(`http://localhost:3001/nft`, nft);
-    console.log(response)
+    console.log(response);
   };
 
   const handleChange = (e) => {
     setNft({ ...nft, [e.target.name]: e.target.value });
-    console.log(nft)
+    console.log(nft);
   };
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="NFT Name"
-          name="nftName"
-          value={nft.nftName}
-          onChange={handleChange}
-        ></input>
+    <>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="NFT Name"
+            name="nftName"
+            value={nft.nftName}
+            onChange={handleChange}
+          ></input>
 
-        <input
-          placeholder="Client secret"
-          name="clientSecret"
-          value={nft.clientSecret}
-          onChange={handleChange}
-        ></input>
+          <input
+            placeholder="Client secret"
+            name="clientSecret"
+            value={nft.clientSecret}
+            onChange={handleChange}
+          ></input>
 
-        <input
-          placeholder="Project ID"
-          name="projectId"
-          value={nft.projectId}
-          onChange={handleChange}
-        ></input>
+          <input
+            placeholder="Project ID"
+            name="projectId"
+            value={nft.projectId}
+            onChange={handleChange}
+          ></input>
 
-        <input
-          placeholder="Email"
-          name="email"
-          value={nft.email}
-          onChange={handleChange}
-        ></input>
+          <input
+            placeholder="Email"
+            name="email"
+            value={nft.email}
+            onChange={handleChange}
+          ></input>
 
-        <input
-          placeholder="Name"
-          name="name"
-          value={nft.name}
-          onChange={handleChange}
-        ></input>
+          <input
+            placeholder="Name"
+            name="name"
+            value={nft.name}
+            onChange={handleChange}
+          ></input>
 
-        <input
-          placeholder="Description"
-          name="description"
-          value={nft.description}
-          onChange={handleChange}
-        ></input>
+          <input
+            placeholder="Description"
+            name="description"
+            value={nft.description}
+            onChange={handleChange}
+          ></input>
 
-        <input
-          placeholder="Type"
-          name="traitType"
-          value={nft.traitType}
-          onChange={handleChange}
-        ></input>
+          <input
+            placeholder="Type"
+            name="traitType"
+            value={nft.traitType}
+            onChange={handleChange}
+          ></input>
 
-        <input
-          placeholder="true or false"
-          name="value"
-          value={nft.value}
-          onChange={handleChange}
-        ></input>
+          <input
+            placeholder="true or false"
+            name="value"
+            value={nft.value}
+            onChange={handleChange}
+          ></input>
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+          <UploadImage />
+          <button style={{backgroundColor: "blue", color: "white", borderRadius: "5px"}} type="submit">Submit</button>
+        </form>
+      </div>
+    </>
   );
 }
 
---
+++ client/src/components/UploadImage.jsx
@@ -0,0 +1,11 @@
+import "./UploadImage.css"
+
+const UploadImage = () => {
+  return (
+    <div className='upload'>
+      <button>UPLOAD Images</button>
+    </div>
+  )
+}
+
+export default UploadImage
